1c921c8367dec28e304e514760eb63a8cfe378c0
ggongddudduddeong <92015258+chlsujeong12@users.noreply.github.com>
2025-06-29 02:33:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // ⭐ 반드시 추가
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ⭐ @를 src로 지정
+      '@': '/src',  // Node 코드 불필요, Vite가 내부적으로 해석
     },
   },
 });
